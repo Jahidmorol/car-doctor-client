43251d031966a342ea9bde03de9ee280dffdb0db
jahid morol <jahidmorol2@gmail.com>
2023-05-11 14:47:44 +0600

    added sign up sectio with responsive

--- src/pages/login/SignIn.jsx
+++ src/pages/login/SignIn.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-import signin from '../../assets/images/login/login.svg'
+import { Link } from "react-router-dom";
+import signin from "../../assets/images/login/login.svg";
 
 const SignIn = () => {
+    
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password);
+
+    }
+
+
   return (
     <div className="hero min-h-[32rem] rounded mt-10 bg-base-200">
       <div className="hero-content md:gap-8 flex-col lg:flex-row">
@@ -11,34 +25,56 @@ const SignIn = () => {
         <div className="card flex-shrink-0 max-w-sm shadow-2xl bg-base-100 md:w-1/2">
           <div className="card-body">
             <h1 className="text-3xl font-bold text-center">Login now!</h1>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="text"
-                placeholder="password"
-                className="input input-bordered"
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn bg-[#FF3811]">Login</button>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn bg-[#FF3811]"
+                />
+              </div>
+            </form>
+            <p className="text-center my-3">
+              New in CarDoctor? <Link to='/login/signup' className="text-[#FF3811]">Sign Up</Link>
+            </p>
           </div>
         </div>
       </div>
--- src/pages/login/SignUp.jsx
+++ src/pages/login/SignUp.jsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import login from '../../assets/images/login/login.svg'
 
 const SignUp = () => {
-    return (
-        <div>
-            its signUp component
+
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+    }
+
+
+  return (
+    <div className="hero min-h-[32rem] rounded mt-10 bg-base-200">
+      <div className="hero-content md:gap-8 flex-col lg:flex-row">
+        <div className="text-center lg:text-left md:w-1/2">
+          <img src={login} alt="login" />
         </div>
-    );
+        <div className="card flex-shrink-0 max-w-sm shadow-2xl bg-base-100 md:w-1/2">
+          <div className="card-body">
+            <h1 className="text-3xl font-bold text-center">Sign Up!</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Sign Up"
+                  className="btn bg-[#FF3811]"
+                />
+              </div>
+            </form>
+            <p className="text-center my-3">
+              Have an account?{" "}
+              <Link to="/login" className="text-[#FF3811]">
+                Sign In
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
